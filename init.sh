7d2d816df5d6ae7f530654a581fa799e728b2b43
Stanisław Drozd <drozdziak1@gmail.com>
2016-10-31 15:18:44 +0100

    update busybox, init.sh and vmroot_desc

--- init.sh
+++ init.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+
+# BusyBox bootstrap
+/bin/busybox --install -s /bin
+
+#Mount things needed by this script
+mount -t proc proc /proc
+mount -t sysfs sysfs /sys
+mount -t devpts devpts /dev/pts
+
 while true; do
 	/bin/sh
 done
